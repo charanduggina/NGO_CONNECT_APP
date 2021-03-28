102d130929e7511c34c506a22a18f019b3bfdc12
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-03-28 22:18:13 +0530

    Update auth.js

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -1,4 +1,5 @@
   
+  
 const { check } = require('express-validator');
 
 exports.userSignupValidator = [
@@ -13,11 +14,28 @@ exports.userSignupValidator = [
         .isLength({ min: 6 })
         .withMessage('Password must be at least  6 characters long')
 ];
+
 exports.userSigninValidator = [
     check('email')
         .isEmail()
         .withMessage('Must be a valid email address'),
     check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least  6 characters long')
+];
+
+exports.forgotPasswordValidator = [
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+        .withMessage('Must be a valid email address')
+];
+
+exports.resetPasswordValidator = [
+    check('newPassword')
+        .not()
+        .isEmpty()
         .isLength({ min: 6 })
         .withMessage('Password must be at least  6 characters long')
 ];
