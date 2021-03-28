5b65cb57ccb22fe3ecdf3b5d60f03d31dc120b57
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-03-28 22:06:10 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+const nodeMailer = require("nodemailer");
 
 const { sendEmailWithNodemailer } = require("../helpers/email");
  
@@ -101,7 +102,18 @@ exports.signin = (req, res) => {
 
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET, algorithms: ['sha1', 'RS256', 'HS256'], // req.user._id
+    userProperty: "auth"
 });
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: 'Access denied'
+        });
+    }
+    next();
+};
 
 exports.adminMiddleware = (req, res, next) => {
     User.findById({ _id: req.user._id }).exec((err, user) => {
@@ -121,4 +133,66 @@ exports.adminMiddleware = (req, res, next) => {
         next();
     });
 };
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resourse! Access denied'
+        });
+    }
+    next();
+};
 
+exports.forgotPassword = (req, res) => {
+    const { email } = req.body;
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist'
+            });
+        }
+
+        const token = jwt.sign(
+            {_id: user._id},
+            process.env.JWT_RESET_PASSWORD,
+            { expiresIn: "10m" }
+          );
+       
+          const emailData = {
+            from: "charanduggina@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+            to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+            subject: "Password Reset LINK",
+            html: `
+                      <h1>Please use the following link to Reset your account  Password</h1>
+                      <p>http://localhost:3000/auth/password/reset/${token}</p>
+                      <hr />
+                      <p>This email may contain sensitive information</p>
+                      <p>http://localhost:3000</p>
+                  `,
+          };
+       
+          const transporter = nodeMailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth: {
+              user: "charanduggina@gmail.com", // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+              pass: "dsmbtdyhqveupbfy", // MAKE SURE THIS PASSWORD IS YOUR GMAIL APP PASSWORD WHICH YOU GENERATED EARLIER
+            },
+            tls: {
+              ciphers: "SSLv3",
+            },
+          });
+         
+          return transporter
+            .sendMail(emailData)
+            .then((info) => {
+              console.log(`Message sent: ${info.response}`);
+              return res.json({
+                message: `Email has been sent to your email. Follow the instruction to activate your account`,
+              });
+            })
+            .catch((err) => console.log(`Problem sending email: ${err}`));
+    });
+};
