898122ae03538b769d7f34fcb8ad478a3ac4c345
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-04-25 23:15:26 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 // import controller
-const { signup, accountActivation, signin } = require('../controllers/auth');
+const { signup, accountActivation, signin, forgotPassword, resetPassword } = require('../controllers/auth');
 
 // import validators
-const { userSignupValidator, userSigninValidator } = require('../validators/auth');
+const {
+    userSignupValidator,
+    userSigninValidator,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require('../validators/auth');
 const { runValidation } = require('../validators');
 
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/account-activation', accountActivation);
 router.post('/signin', userSigninValidator, runValidation, signin);
+// forgot reset password
+//router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword);
+//router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
+
 
 module.exports = router;
