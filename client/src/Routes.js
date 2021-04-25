8ae93f9a974b8b6993a271a38a9e450a8e10fb33
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-04-25 22:35:48 +0530

    Update Routes.js

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -13,6 +13,16 @@ import AddProduct from './admin/AddNgo';
 import Home from './core/Home';
 import Ngocard from './core/Ngocard';
 import Ngodetails from './core/Ngodetails';
+import Volunteer from './core/Volunteer';
+import Donatepage from './core/Donate';
+import Payment from './core/payment';
+import ManageProducts from './admin/ManageProducts';
+import UpdateNgo from './admin/UpdateNgo';
+import Orders from './admin/Orders';
+import Userhistory from './core/Userhistory';
+import statsadmin from './core/statsadmin';
+import indexRoutes from './src/routes/index.jsx';      
+import "./src/assets/scss/style.css";                
 const Routes = () => {
     return (
         <BrowserRouter>
@@ -24,9 +34,24 @@ const Routes = () => {
                 <PrivateRoute path="/private" exact component={Private} />
                 <PrivateRoute path="/NgoCard" exact component={Ngocard} />
                 <PrivateRoute path="/Ngo/:productId" exact component={Ngodetails} />
+                <PrivateRoute path="/Ngo/volunteer/:productId" exact component={Volunteer} />
+                <PrivateRoute path="/Ngo/donate/:productId" exact component={Donatepage} />
+                <PrivateRoute path="/amount/donate/:productId" exact component={Payment} />
+                <PrivateRoute path="/User/Donations" exact component={Userhistory} />
                 <AdminRoute path="/admin" exact component={Admin} />
                 <AdminRoute path="/create/category" exact component={AddCategory} />
                 <AdminRoute path="/create/ngo" exact component={AddProduct} />
+                <AdminRoute path="/admin/Ngo" exact component={ManageProducts} />
+                <AdminRoute path="/admin/ngo/update/:productId" exact component={UpdateNgo} />
+                <AdminRoute path="/admin/Donations" exact component={Orders} />
+                <Route path="/stats" exact component={statsadmin} />
+                <Route exact path="/dashboard">
+          {indexRoutes.map((prop, key) => {
+            return (
+              <Route path={prop.path} key={key} component={prop.component} />
+            );
+          })}
+        </Route>
             </Switch>
         </BrowserRouter>
     );
