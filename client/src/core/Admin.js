04a74476c99e79e3986f79cdb2241ce384f6fadf
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-04-25 22:37:25 +0530

    Update Admin.js

--- client/src/core/Admin.js
+++ client/src/core/Admin.js
@@ -1,151 +1,165 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import Layout from '../core/Layout';
+import { API } from "../config";
+import queryString from "query-string";
 import axios from 'axios';
-import { isAuth, getCookie, signout, updateUser } from '../auth/helpers';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
 
-const Admin = ({ history }) => {
-    const [values, setValues] = useState({
-        role: '',
-        name: '',
-        email: '',
-        password: '',
-        buttonText: 'Submit'
-    });
-
-    const token = getCookie('token');
-
-    useEffect(() => {
-        loadProfile();
-    }, []);
-
-    const loadProfile = () => {
-        axios({
-            method: 'GET',
-            url: `${process.env.REACT_APP_API}/user/${isAuth()._id}`,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+export const donateobject = (userId, token,ngoId, donateobjects,name) => {
+    console.log(userId);
+    console.log(ngoId);
+    console.log(donateobjects);
+    console.log(token);
+    return  axios({
+        method: 'POST',
+        url: `${API}/donate/object`,
+        data: { userId,ngoId,donateobjects,name}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
         })
-            .then(response => {
-                console.log('PRIVATE PROFILE UPDATE', response);
-                const { role, name, email } = response.data;
-                setValues({ ...values, role, name, email });
-            })
-            .catch(error => {
-                console.log('PRIVATE PROFILE UPDATE ERROR', error.response.data.error);
-                if (error.response.status === 401) {
-                    signout(() => {
-                        history.push('/');
-                    });
-                }
-            });
-    };
-
-    const { role, name, email, password, buttonText } = values;
-
-    const handleChange = name => event => {
-        // console.log(event.target.value);
-        setValues({ ...values, [name]: event.target.value });
-    };
-
-    const clickSubmit = event => {
-        event.preventDefault();
-        setValues({ ...values, buttonText: 'Submitting' });
-        axios({
-            method: 'PUT',
-            url: `${process.env.REACT_APP_API}/admin/update`,
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            data: { name, password }
+        .catch(err => {
+            console.log(err);
+        });
+};
+export const ratingupdate = (ngoId,rating,count) => {
+    console.log(ngoId);
+    console.log(rating);
+    console.log(count);
+    return  axios({
+        method: 'POST',
+        url: `${API}/ngo/rate`,
+        data: { ngoId,rating,count}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
         })
-            .then(response => {
-                console.log('PRIVATE PROFILE UPDATE SUCCESS', response);
-                updateUser(response, () => {
-                    setValues({ ...values, buttonText: 'Submitted' });
-                    toast.success('Profile updated successfully');
-                });
-            })
-            .catch(error => {
-                console.log('PRIVATE PROFILE UPDATE ERROR', error.response.data.error);
-                setValues({ ...values, buttonText: 'Submit' });
-                toast.error(error.response.data.error);
-            });
-    };
-    
-    const adminLinks = () => {
-        return (
-            <div className="card ">
-                <h4 className="card-header ">Admin Links</h4>
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/create/category">
-                            Create Category
-                        </Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/create/ngo">
-                            Create Ngo page
-                        </Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/admin/orders">
-                            View Donations
-                        </Link>
-                    </li>
-                  
-                </ul>
-            </div>
-        );
-    };
-
-    const updateForm = () => (
-        <form>
-              <p className="lead  ">Profile update</p>
-            <div className="form-group">
-                <label className="text-muted">Role</label>
-                <input defaultValue={role} type="text" className="form-control" disabled />
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} value={name} type="text" className="form-control" />
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input defaultValue={email} type="email" className="form-control" disabled />
-            </div>
-
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} value={password} type="password" className="form-control" />
-            </div>
-
-            <div>
-                <button className="btn btn-primary" onClick={clickSubmit}>
-                    {buttonText}
-                </button>
-            </div>
-        </form>
-    );
-
-    return (
-        <Layout
-        className="container-fluid"
-        title="Dashboard"
-        >
-            <div className="row mt-5">
-                <ToastContainer />
-                <div className="col-4">{adminLinks()}</div>
-                <div className="col-8">{updateForm()}</div>
-              
-               
-            </div>
-        </Layout>
-    );
+        .catch(err => {
+            console.log(err);
+        });
+};
+export const donateamount = (userId, token,ngoId, donateamount,transactionId,name) => {
+    console.log(userId);
+    console.log(ngoId);
+    console.log(donateamount);
+    console.log(token);
+    return  axios({
+        method: 'POST',
+        url: `${API}/donate/money`,
+        data: { userId,ngoId,donateamount,transactionId,name}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
 
-export default Admin;
+export const getProducts = sortBy => {
+    return fetch(`${API}/ngos?sortBy=${sortBy}&order=desc&limit=6`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+    const data = {
+        limit,
+        skip,
+        filters
+    };
+    return fetch(`${API}/ngos/by/search`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+
+export const list = params => {
+    const query = queryString.stringify(params);
+    console.log("query", query);
+    return fetch(`${API}/ngos/search?${query}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const read = productId => {
+    return fetch(`${API}/ngo/${productId}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
