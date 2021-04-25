f4c4c1eed1948ae595f8f09e652b728ed538c817
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-04-25 23:20:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // connect to db
 mongoose
@@ -21,6 +24,10 @@ mongoose
 // import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const ngoRoutes = require('./routes/ngo');
+const donateRoutes = require('./routes/donate');
+const braintreeRoutes = require('./routes/braintree');
 
 // app middlewares
 app.use(morgan('dev'));
@@ -30,9 +37,20 @@ if ((process.env.NODE_ENV = 'development')) {
     app.use(cors({ origin: `http://localhost:3000` }));
 }
 
+
 // middleware
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
+app.use('/api',categoryRoutes);
+app.use('/api',ngoRoutes);
+app.use('/api',donateRoutes);
+app.use('/api',braintreeRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
+
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
