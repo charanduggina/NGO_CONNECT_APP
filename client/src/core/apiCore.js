8169658ac059440d332d47480dbdde1e717406c9
charanduggina <65570085+charanduggina@users.noreply.github.com>
2021-04-25 23:39:10 +0530

    Add files via upload

--- client/src/core/apiCore.js
+++ client/src/core/apiCore.js
@@ -1,6 +1,57 @@
 import { API } from "../config";
 import queryString from "query-string";
+import axios from 'axios';
 
+export const donateobject = (userId, token,ngoId, donateobjects,name) => {
+    console.log(userId);
+    console.log(ngoId);
+    console.log(donateobjects);
+    console.log(token);
+    return  axios({
+        method: 'POST',
+        url: `${API}/donate/object`,
+        data: { userId,ngoId,donateobjects,name}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+export const ratingupdate = (ngoId,rating,count) => {
+    console.log(ngoId);
+    console.log(rating);
+    console.log(count);
+    return  axios({
+        method: 'POST',
+        url: `${API}/ngo/rate`,
+        data: { ngoId,rating,count}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+export const donateamount = (userId, token,ngoId, donateamount,transactionId,name) => {
+    console.log(userId);
+    console.log(ngoId);
+    console.log(donateamount);
+    console.log(token);
+    return  axios({
+        method: 'POST',
+        url: `${API}/donate/money`,
+        data: { userId,ngoId,donateamount,transactionId,name}
+       //data: {donateobjects}
+    }).then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 
 export const getProducts = sortBy => {
     return fetch(`${API}/ngos?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -60,6 +111,52 @@ export const list = params => {
 export const read = productId => {
     return fetch(`${API}/ngo/${productId}`, {
         method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getPurchaseHistory = (userId, token) => {
+    return fetch(`${API}/orders/by/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
     })
         .then(response => {
             return response.json();
